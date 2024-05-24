4ed2b507dc66ca3e048fa507c46c51218453dd17
Vope17 <kkeea1020@gmail.com>
2024-05-24 16:48:50 +0800

    week13

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,6 @@
+/**  
+* @author Vope <kkeea1020@gmail.com>
+*/
 import Link from "next/link";
 import {
   Navbar,
@@ -16,6 +19,34 @@ import {
 } from "flowbite-react";
 
 export default function Home() {
+  const items = 
+  [
+    {
+      cover:"/banner/rainbowEye.jpg",
+      name:"彩虹眼",
+      description: "雲林雨後的彩虹與農田相互輝映",
+    },
+    {
+      cover:"/banner/yuntech.jpg",
+      name:"雲林科技大學",
+      description: "位於斗六的雲科大",
+    },
+    {
+      cover:"/banner/mountain.jpg",
+      name:"華山",
+      description: "雲林華山",
+    },
+    {
+      cover:"/banner/green.jpg",
+      name:"綠色隧道",
+      description: "雲林綠色隧道，假日有攤販",
+    },
+    {
+      cover:"/banner/station.jpg",
+      name:"雲林斗六車站",
+      description: "雲林火車站",
+    },
+  ]
   return (
     <>
       <div className="bg-cyan-800">
@@ -59,26 +90,27 @@ export default function Home() {
       </div>
       <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
         <Carousel>
-          <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+          <img src="/banner/rainbowEye.jpg" alt="彩虹眼 由 蔡佩書拍攝" />
+          <img src="/banner/yuntech.jpg" alt="由 Zhuang kuonan - 自己的作品, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=65669091"/>
+          <img src="/banner/green.jpg" alt="..." />
+          <img src="/banner/mountain.jpg" alt="..." />
+          <img src="/banner/station.jpg" alt="..." />
         </Carousel>
       </div>
 
-      <div className="bg-white">
-        <div className="container mx-auto">
+      <div className="bg-white py-16">
+        <div className="container mx-auto grid grid-cols-4 gap-4">
+          { items.map(item =>
           <Card
             className="max-w-sm"
             imgAlt="Meaningful alt text for an image that is not purely decorative"
-            imgSrc="/images/blog/image-1.jpg"
+            imgSrc={item.cover}
           >
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
+              {item.name}
             </h5>
             <p className="font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+              {item.description}
             </p>
             <Button>
               Read more
@@ -91,6 +123,7 @@ export default function Home() {
               </svg>
             </Button>
           </Card>
+          )}
         </div>
       </div>
 
